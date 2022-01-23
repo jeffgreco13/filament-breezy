e3217726a289bc6ca1feac9696294665dcf29964
jeffgreco13 <jeffgreco13@users.noreply.github.com>
2022-01-23 23:11:23 +0000

    Fix styling

--- src/FilamentBreezy.php
+++ src/FilamentBreezy.php
@@ -2,9 +2,6 @@
 
 namespace JeffGreco13\FilamentBreezy;
 
-use JeffGreco13\FilamentBreezy\Events\UserInvitedToTeam;
-use JeffGreco13\FilamentBreezy\Models\TeamInvite;
-
 class FilamentBreezy
 {
     //

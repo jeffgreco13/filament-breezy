6ce037b87dea02196efa29c0c05535ca2b59f395
Jean-Vincent kassi <jeanvincent45@gmail.com>
2022-01-24 15:13:03 +0000

    Add french translations

---
+++ resources/lang/fr/default.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    "registration" => [
+        "title" => "S'inscrire",
+        "heading" => "Créer un nouveau compte",
+        "submit" => [
+            "label" => "S'inscrire",
+        ],
+    ],
+    "reset_password" => [
+        "title" => "Mot de passe oublié",
+        "heading" => "Réinitialisez votre mot de passe",
+        "submit" => [
+            "label" => "Envoyer un e-mail de réinitialisation",
+        ],
+    ],
+    "verification" => [
+        "title" => "Vérifier votre e-mail",
+        "heading" => "Vérification de l'e-mail requise",
+        "submit" => [
+            "label" => "Déconnexion",
+        ],
+    ],
+];

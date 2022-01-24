4868caa38c7b0430cb1ab656b28c28c190c69111
jeffgreco13 <jeffgreco13@users.noreply.github.com>
2022-01-24 18:52:30 +0000

    Fix styling

--- src/Http/Controllers/EmailVerificationController.php
+++ src/Http/Controllers/EmailVerificationController.php
@@ -2,22 +2,22 @@
 
 namespace JeffGreco13\FilamentBreezy\Http\Controllers;
 
-use Illuminate\Routing\Controller;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class EmailVerificationController extends Controller
 {
     public function __invoke(string $id, string $hash): RedirectResponse
     {
-        if (!hash_equals((string) $id, (string) Auth::user()->getKey())) {
+        if (! hash_equals((string) $id, (string) Auth::user()->getKey())) {
             throw new AuthorizationException();
         }
 
         if (
-            !hash_equals(
+            ! hash_equals(
                 (string) $hash,
                 sha1(Auth::user()->getEmailForVerification())
             )

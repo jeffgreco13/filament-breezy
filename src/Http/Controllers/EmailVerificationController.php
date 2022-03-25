0da21364e698d88d42f7063d9451223f1e71b383
Jeff Greco <jeff@jeffpgreco.com>
2022-03-25 17:22:12 -0400

    fix: remove getKey() references
    Switch to ->id() and auth()->user() helper in EmailVerificationController

--- src/Http/Controllers/EmailVerificationController.php
+++ src/Http/Controllers/EmailVerificationController.php
@@ -6,31 +6,30 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Auth;
 
 class EmailVerificationController extends Controller
 {
     public function __invoke(string $id, string $hash): RedirectResponse
     {
-        if (! hash_equals((string) $id, (string) Auth::user()->getKey())) {
+        if (! hash_equals((string) $id, (string) auth()->id())) {
             throw new AuthorizationException();
         }
 
         if (
             ! hash_equals(
                 (string) $hash,
-                sha1(Auth::user()->getEmailForVerification())
+                sha1(auth()->user()->getEmailForVerification())
             )
         ) {
             throw new AuthorizationException();
         }
 
-        if (Auth::user()->hasVerifiedEmail()) {
+        if (auth()->user()->hasVerifiedEmail()) {
             return redirect(config("filament.home_url"));
         }
 
-        if (Auth::user()->markEmailAsVerified()) {
-            event(new Verified(Auth::user()));
+        if (auth()->user()->markEmailAsVerified()) {
+            event(new Verified(auth()->user()));
         }
 
         return redirect(config("filament.home_url"));

26094ef0990122b4f76d841858bcfd7bc84c0036
Jean-Vincent kassi <jeanvincent45@gmail.com>
2022-04-11 19:54:51 +0000

    Feature: Enable login with another column in the user table

--- src/Http/Livewire/Auth/Login.php
+++ src/Http/Livewire/Auth/Login.php
@@ -2,13 +2,73 @@
 
 namespace JeffGreco13\FilamentBreezy\Http\Livewire\Auth;
 
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
 use Filament\Http\Livewire\Auth\Login as FilamentLogin;
 use Filament\Http\Livewire\Concerns\CanNotify;
+use Filament\Http\Responses\Auth\Contracts\LoginResponse;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\TextInput;
+use Filament\Facades\Filament;
 use Illuminate\Contracts\View\View;
+
 
 class Login extends FilamentLogin
 {
     use CanNotify;
+
+
+    public function authenticate(): ?LoginResponse
+    {
+
+        $data = $this->form->getState();
+
+        // login field
+        $fieldType  = config('filament-breezy.fallback_login_field') == 'email' ? 'email' : 'login';
+        // user column 
+        $loginColumn = filter_var($data[$fieldType], FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        try {
+            $this->rateLimit(5);
+        } catch (TooManyRequestsException $exception) {
+            $this->addError($fieldType, __('filament::login.messages.throttled', [
+                'seconds' => $exception->secondsUntilAvailable,
+                'minutes' => ceil($exception->secondsUntilAvailable / 60),
+            ]));
+
+            return null;
+        }
+
+        if (!Filament::auth()->attempt([
+            $loginColumn => $data[$fieldType],
+            'password' => $data['password'],
+        ], $data['remember'])) {
+            $this->addError($fieldType, __('filament::login.messages.failed'));
+
+            return null;
+        }
+
+        return app(LoginResponse::class);
+    }
+
+
+    protected function getFormSchema(): array
+    {
+        return config('filament-breezy.fallback_login_field') == 'email' ?
+        parent::getFormSchema() : [
+            TextInput::make('login')
+            ->label(__('filament-breezy::default.fields.login'))
+            ->required()
+            ->autocomplete(),
+            TextInput::make('password')
+            ->label(__('filament::login.fields.password.label'))
+            ->password()
+                ->required(),
+            Checkbox::make('remember')
+            ->label(__('filament::login.fields.remember.label')),
+        ];
+    }
+
+
 
     public function mount(): void
     {

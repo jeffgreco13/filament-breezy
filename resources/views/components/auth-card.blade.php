ba01e0e0a521f94b31bcf76e878f4d9aae9192e4
Adam Weston <awcodes1@gmail.com>
2022-05-08 18:11:29 -0400

    feat: updates styles to match filament's new login styles

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,11 +1,18 @@
 @props(['action'])
-<div @class(['flex items-center justify-center min-h-screen bg-gray-100 text-gray-900 filament-breezy-auth-component','dark:bg-gray-900 dark:text-white' => config('filament.dark_mode')])>
+<div @class([
+    'flex items-center justify-center min-h-screen bg-gray-100 text-gray-900 filament-breezy-auth-component filament-login-page',
+    'dark:bg-gray-900 dark:text-white' => config('filament.dark_mode'),
+])>
 
     <x-filament::notification-manager />
 
-    <div class="p-2 max-w-{{config('filament-breezy.auth_card_max_w')??'md'}} space-y-8 w-screen">
-        <form wire:submit.prevent="{{$action}}" @class(['p-8 space-y-8 bg-white border border-gray-300 shadow rounded-2xl filament-breezy-auth-card', 'dark:bg-gray-800 dark:border-gray-700' => config('filament.dark_mode')])>
-            {{$slot}}
+    <div
+        class="px-6 -mt-16 md:mt-0 md:px-2 max-w-{{ config('filament-breezy.auth_card_max_w') ?? 'md' }} space-y-8 w-screen">
+        <form wire:submit.prevent="{{ $action }}" @class([
+            'p-8 space-y-8 bg-white/50 backdrop-blur-xl border border-gray-200 shadow-2xl rounded-2xl relative filament-breezy-auth-card',
+            'dark:bg-gray-900/50 dark:border-gray-700' => config('filament.dark_mode'),
+        ])>
+            {{ $slot }}
         </form>
         <x-filament::footer />
     </div>

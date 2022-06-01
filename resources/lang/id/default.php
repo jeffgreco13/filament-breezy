fb807371d11409ef0081d0e4a7f9a4d1c4387074
Risang Baskoro <git@risangbaskoro.com>
2022-06-02 04:02:35 +0700

    Update Translatable Template

--- resources/lang/id/default.php
+++ resources/lang/id/default.php
@@ -2,85 +2,130 @@
 
 return [
     "login" => [
-        "forgot_password_link" => "Lupa kata sandi?",
-        "create_an_account" => "Buat akun baru",
+        "username_or_email" => "Username ou Email",
+        "forgot_password_link" => "Forgot password?",
+        "create_an_account" => "create an account",
+    ],
+    "two_factor" => [
+        "heading" => "Two Factor Challenge",
+        "description" => "Please confirm access to your account by entering the authentication code provided by your authenticator application.",
+        "code_placeholder" => "XXX-XXX",
+        "recovery" => [
+            "heading" => "Two Factor Challenge",
+            "description" => "Please confirm access to your account by entering one of your emergency recovery codes.",
+        ],
+        "recovery_code_placeholder" => "abcdef-98765",
+        "recovery_code_text" => "Lost device?",
+        "recovery_code_link" => "Use a recovery code",
+        "back_to_login_link" => "Back to login"
     ],
     "registration" => [
-        "title" => "Daftar",
-        "heading" => "Buat akun baru",
+        "title" => "Register",
+        "heading" => "Create a new account",
         "submit" => [
-            "label" => "Daftar",
+            "label" => "Sign up",
         ],
-        "notification_unique" =>
-            "Akun dengan email ini sudah terdaftar. Silahkan coba login kembali.",
+        "notification_unique" => "An account with this email already exists. Please login.",
     ],
     "reset_password" => [
-        "title" => "Lupa kata sandi",
-        "heading" => "Setel ulang kata sandi",
+        "title" => "Forgot password",
+        "heading" => "Reset your password",
         "submit" => [
-            "label" => "Kirim",
+            "label" => "Submit",
         ],
-        "notification_error" => "Error: silahkan coba kembali.",
-        "notification_success" => "Coba cek instruksi di inbox email anda!",
+        "notification_error" => "Error: please try again later.",
+        "notification_success" => "Check your inbox for instructions!",
     ],
     "verification" => [
-        "title" => "Verifikasi email",
-        "heading" => "Verifikasi email diperlukan",
+        "title" => "Verify email",
+        "heading" => "Email verification required",
         "submit" => [
-            "label" => "Keluar",
+            "label" => "Sign out",
         ],
-        "notification_success" => "Coba cek instruksi di inbox email anda!",
-        "notification_resend" => "Verifikasi email sudah dikirim kembali",
-        "before_proceeding" =>
-            "Sebelum dilanjutkan, silahkan cek email anda untuk tautan verifikasi email.",
-        "not_receive" => "Jika anda tidak menerima email,",
-        "request_another" => "klik disini untuk meminta kembali.",
+        "notification_success" => "Check your inbox for instructions!",
+        "notification_resend" => "Verification email has been resent.",
+        "before_proceeding" => "Before proceeding, please check your email for a verification link.",
+        "not_receive" => "If you did not receive the email,",
+        "request_another" => "click here to request another one",
     ],
     "profile" => [
-        "account" => "Akun",
-        "profile" => "Profil",
-        "my_profile" => "Profil saya",
+        "account" => "Account",
+        "profile" => "Profile",
+        "my_profile" => "My Profile",
         "personal_info" => [
-            "heading" => "Informasi pribadi",
-            "subheading" => "Atur informasi pribadi anda",
+            "heading" => "Personal Information",
+            "subheading" => "Manage your personal information.",
             "submit" => [
-                "label" => "Perbaharui",
+                "label" => "Update",
             ],
-            "notify" => "Profil sukses diperbaharui!",
+            "notify" => "Profile updated successfully!",
         ],
         "password" => [
-            "heading" => "Kata sandi",
-            "subheading" => "harus 8 karakter.",
+            "heading" => "Password",
+            "subheading" => "Must be 8 characters.",
             "submit" => [
-                "label" => "Perbaharui",
+                "label" => "Update",
             ],
-            "notify" => "kata sandi sukses diperbaharui!",
+            "notify" => "Password updated successfully!",
+        ],
+        "2fa" => [
+            "title" => "Two Factor Authentication",
+            "description" => "Manage 2 factor authentication for your account (recommended).",
+            "actions" => [
+                "enable" => "Enable",
+                "regenerate_codes"=>"Regenerate Codes",
+                "disable"=>"Disable",
+                "confirm_finish" => "Confirm & finish",
+                "cancel_setup" => "Cancel setup"
+            ],
+            "setup_key" => "Setup key",
+            "not_enabled" => [
+                "title" => "You have not enabled two factor authentication.",
+                "description"=>"When two factor authentication is enabled, you will be prompted for a secure, random token during authentication. You may retrieve this token from your phone's Google Authenticator application."
+            ],
+            "finish_enabling" => [
+                "title"=>"Finish enabling two factor authentication.",
+                "description" => "To finish enabling two factor authentication, scan the following QR code using your phone's authenticator application or enter the setup key and provide the generated OTP code."
+            ],
+            "enabled"=>[
+                "title"=>"You have enabled two factor authentication!",
+                "description"=>"Two factor authentication is now enabled. Scan the following QR code using your phone's authenticator application or enter the setup key.",
+                "store_codes"=>"Store these recovery codes in a secure password manager. They can be used to recover access to your account if your two factor authentication device is lost.",
+                "show_codes"=>'Show Recovery Codes',
+                "hide_codes" => 'Hide Recovery Codes'
+            ],
+            "confirmation" => [
+                "success_notification" => 'Code verified. Two factor authentication enabled.',
+                "invalid_code" => "The code you have entered is invalid."
+            ]
         ],
         "sanctum" => [
-            "title" => "Token API",
-            "description" =>
-                "Mengelola token API yang memungkinkan layanan pihak ketiga mengakses aplikasi ini atas nama Anda. CATATAN: token Anda ditampilkan sekali setelah dibuat. Jika Anda kehilangan token, Anda harus menghapusnya dan membuat yang baru. ",
+            "title" => "API Tokens",
+            "description" => "Manage API tokens that allow third-party services to access this application on your behalf. NOTE: your token is shown once upon creation. If you lose your token, you will need to delete it and create a new one.",
             "create" => [
-                "notify" => "Token sukses dibuat!",
+                "notify" => "Token created successfully!",
                 "submit" => [
-                    "label" => "Buat",
+                    "label" => "Create",
                 ],
             ],
             "update" => [
-                "notify" => "Token sukses diperbaharui!",
+                "notify" => "Token updated successfully!",
             ],
         ],
     ],
     "fields" => [
         "email" => "Email",
-        "login" => "Pengguna",
-        "name" => "Nama",
-        "password" => "Kata Sandi",
-        "password_confirm" => "Konfirmasi Kata Sandi",
-        "new_password" => "Kata Sandi Baru",
-        "new_password_confirmation" => "Konfirmasi Kata Sandi baru",
-        "token_name" => "Nama Token",
-        "abilities" => "Kemampuan",
+        "login" => "Login",
+        "name" => "Name",
+        "password" => "Password",
+        "password_confirm" => "Password confirm",
+        "new_password" => "New password",
+        "new_password_confirmation" => "Confirm password",
+        "token_name" => "Token name",
+        "abilities" => "Abilities",
+        "2fa_code" => "Code",
+        "2fa_recovery_code" => "Recovery Code"
     ],
     "or" => "Or",
+    "cancel" => "Cancel"
 ];

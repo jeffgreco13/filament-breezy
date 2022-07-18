aa037509640e09709148a949be7d932d3f064099
bfiessinger <bastianfiessinger1@yahoo.de>
2022-07-18 12:13:06 +0200

    fix: use the correct model for Sanctum PersonalAccessToken

--- src/Http/Livewire/BreezySanctumTokens.php
+++ src/Http/Livewire/BreezySanctumTokens.php
@@ -22,7 +22,7 @@ class BreezySanctumTokens extends Component implements Tables\Contracts\HasTable
 
     protected function getTableQuery(): Builder
     {
-        return PersonalAccessToken::where([
+        return app(Sanctum::$personalAccessTokenModel)->where([
             ["tokenable_id", '=', Filament::auth()->id()],
             ["tokenable_type", '=', config('filament-breezy.user_model')]
         ]);

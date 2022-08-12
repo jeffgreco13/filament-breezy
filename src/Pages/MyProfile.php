798e63cd6d18b585991848e8425d4ee3807bbc1b
Ashley Hood <ashley@opensauce.systems>
2022-08-12 14:42:59 +0100

    add user provider model to profile form
    This will allow form fields with relationships to be added to the `updateProfileForm`.

--- src/Pages/MyProfile.php
+++ src/Pages/MyProfile.php
@@ -42,6 +42,7 @@ class MyProfile extends Page
     {
         return array_merge(parent::getForms(), [
             "updateProfileForm" => $this->makeForm()
+                ->model(config('auth.providers.users.model'))
                 ->schema($this->getUpdateProfileFormSchema())
                 ->statePath('userData'),
             "updatePasswordForm" => $this->makeForm()->schema(

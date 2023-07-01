5c3c1f3825317227755feac9b28acbffe993cfc5
Anthony Schuijlenburg <a.schuijlenburg@gmail.com>
2023-07-01 09:21:50 +0200

    Update Dutch translations
    I noticed an error where enabling 2fa read "2fa is NOT enabled", while fixing this, I edited some other translations and added missing ones.

--- resources/lang/nl/default.php
+++ resources/lang/nl/default.php
@@ -17,7 +17,7 @@ return [
         "code_placeholder" => "XXX-XXX",
         "recovery" => [
             "heading" => "Twee-factorenauthenticatie",
-            "description" => "Bevestig de toegang tot je account door een van je noodherstelcodes in te voeren.",
+            "description" => "Bevestig de toegang tot je account door één van je noodherstelcodes in te voeren.",
         ],
         "recovery_code_placeholder" => "abcdef-98765",
         "recovery_code_text" => "Apparaat kwijt?",
@@ -26,9 +26,9 @@ return [
     ],
     "registration" => [
         "title" => "Registreren",
-        "heading" => "Nieuwe account aanmaken",
+        "heading" => "Nieuw account aanmaken",
         "submit" => [
-            "label" => "Inloggen",
+            "label" => "Aanmelden",
         ],
         "notification_unique" => "Een account met dit e-mailaddres bestaat al. Graag inloggen.",
     ],
@@ -38,8 +38,8 @@ return [
         "submit" => [
             "label" => "Verzenden",
         ],
-        "notification_error" => "Error: probeer het later nogmaals.",
-        "notification_error_link_text"=>"Try Again",
+        "notification_error" => "Fout bij het resetten van het wachtwoord, probeer het later nogmaals.",
+        "notification_error_link_text"=>"Probeer opnieuw",
         "notification_success" => "Controleer je e-mail voor instructies!",
     ],
     "verification" => [
@@ -75,7 +75,7 @@ return [
             "notify" => "Wachtwoord succesvol bijgewerkt!",
         ],
         "2fa" => [
-            "title" => "Twee-factorenauthenticatie",
+            "title" => "Tweefactorenauthenticatie",
             "description" => "Beheer 2-factor authenticatie voor je account (aanbevolen).",
             "actions" => [
                 "enable" => "Inschakelen",
@@ -87,16 +87,16 @@ return [
             "setup_key" => "Sleutel: ",
             "not_enabled" => [
                 "title" => "Je hebt tweefactorauthenticatie niet ingeschakeld.",
-                "description"=>"Wanneer tweefactorauthenticatie is ingeschakeld, word je tijdens de authenticatie om een veilige, willekeurige token gevraagd. Je kunt deze token ophalen uit de Google Authenticator-app van je telefoon."
+                "description"=>"Wanneer tweefactorauthenticatie is ingeschakeld, word je tijdens de authenticatie om een veilige, willekeurige token gevraagd. Je kunt deze token ophalen uit bijvoorbeeld de Google Authenticator-app van je telefoon."
             ],
             "finish_enabling" => [
                 "title"=>"Voltooi het inschakelen van tweefactorauthenticatie.",
                 "description" => "Om het inschakelen van tweefactorauthenticatie te voltooien, scan je de volgende QR-code met behulp van de authenticatietoepassing van je telefoon of voer je de configuratiesleutel in en geef je de gegenereerde OTP-code op."
             ],
             "enabled" => [
-                "title"=>"Je hebt tweefactorauthenticatie niet ingeschakeld!",
-                "description" => "Tweefactorauthenticatie is nu ingeschakeld. Scan de volgende QR-code met de authenticatietoepassing van je telefoon of voer de configuratiesleutel in.",
-                "store_codes" => "Bewaar deze herstelcodes in een veilige wachtwoordbeheerder. Ze kunnen worden gebruikt om de toegang tot je account te herstellen als je apparaat voor tweefactorauthenticatie verloren is gegaan.",
+                "title"=>"Je hebt tweefactorauthenticatie ingeschakeld!",
+                "description" => "Tweefactorauthenticatie is nu ingeschakeld. Dat bevordert de veiligheid van je account.",
+                "store_codes" => "Bewaar deze herstelcodes in een veilige wachtwoordbeheerder. Ze kunnen worden gebruikt om de toegang tot je account te herstellen als je geen toegang meer hebt tot je apparaat voor tweefactorauthenticatie.",
                 "show_codes" => "Toon herstelcodes",
                 "hide_codes" => "Herstelcodes verbergen"
             ],
@@ -107,7 +107,7 @@ return [
         ],
         "sanctum" => [
             "title" => "API Tokens",
-            "description" => "Beheer API tokens voor externe-toegang tot je account. BELANGRIJK: je token is eenmalig zichbaar na aanmaken. Indien je je token vergeet zul je deze moeten verwijderen en een nieuwe aanmaken.",
+            "description" => "Beheer API tokens voor externe-toegang tot je account. LET OP: je token is eenmalig zichbaar na aanmaken. Indien je je token vergeet zul je deze moeten verwijderen en een nieuwe moeten aanmaken.",
             "create" => [
                 "notify" => "Token succesvol aangemaakt!",
                 "submit" => [
@@ -128,6 +128,9 @@ return [
         "new_password_confirmation" => "Nieuw wachtwoord bevestigen",
         "token_name" => "Token naam",
         "abilities" => "Mogelijkheden",
+        "2fa_code" => "Code",
+        "2fa_recovery_code" => "Herstelcode"
     ],
     "or" => "Of",
+    "cancel" => "Annuleren"
 ];

d4182483956a91df970791a3592270ba75d5391a
Mahmudul Hasan <mahmudbappy.pri@gmail.com>
2024-01-12 23:24:46 +0000

    ui: fix divider color in dark mode

--- resources/views/filament/pages/my-profile.blade.php
+++ resources/views/filament/pages/my-profile.blade.php
@@ -1,5 +1,5 @@
 <x-filament::page>
-    <div class="space-y-6 divide-y divide-gray-900/10">
+    <div class="space-y-6 divide-y divide-gray-900/10 dark:divide-white/10">
         @foreach ($this->getRegisteredMyProfileComponents() as $component)
             @unless(is_null($component))
                 @livewire($component)

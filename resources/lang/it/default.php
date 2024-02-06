4b637dad08bc7ee6b6b56f40325f2f2e4006a835
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2024-02-06 21:15:17 +0100

    IT Missing Italian Translation

--- resources/lang/it/default.php
+++ resources/lang/it/default.php
@@ -58,6 +58,7 @@ return [
         'account' => 'Account',
         'profile' => 'Profilo',
         'my_profile' => 'Il mio profilo',
+        'subheading' => 'Gestisci il tuo profilo.',
         'personal_info' => [
             'heading' => 'Informazioni personali',
             'subheading' => 'Gestisci le tue informazioni personali.',

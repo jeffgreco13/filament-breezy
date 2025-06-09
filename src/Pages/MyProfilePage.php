230cb27e4885fdf76e4563a0fb22f28336291c9e
jeffgreco13 <12453974+jeffgreco13@users.noreply.github.com>
2025-06-09 13:52:56 +0000

    Fix styling

--- src/Pages/MyProfilePage.php
+++ src/Pages/MyProfilePage.php
@@ -11,17 +11,17 @@ class MyProfilePage extends Page
 
     protected static string $view = 'filament-breezy::filament.pages.my-profile';
 
-    public function getTitle(): string | Htmlable
+    public function getTitle(): string|Htmlable
     {
         return __('filament-breezy::default.profile.my_profile');
     }
 
-    public function getHeading(): string | Htmlable
+    public function getHeading(): string|Htmlable
     {
         return __('filament-breezy::default.profile.my_profile');
     }
 
-    public function getSubheading(): string | Htmlable | null
+    public function getSubheading(): string|Htmlable|null
     {
         return __('filament-breezy::default.profile.subheading') ?? null;
     }

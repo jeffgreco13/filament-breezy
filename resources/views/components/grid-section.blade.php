5115e212af87f7865b5be0843d7a3bb77975114f
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2025-06-15 02:00:25 +0200

    Fix

--- resources/views/components/grid-section.blade.php
+++ resources/views/components/grid-section.blade.php
@@ -1,16 +1,16 @@
 @props(['title','description'])
-<x-grid @class(["pt-6 gap-4 filament-breezy-grid-section"]) {{ $attributes }}>
+<x-filament-breezy::grid @class(["pt-6 gap-4 filament-breezy-grid-section"]) {{ $attributes }}>
 
-    <x-grid.column>
+    <x-filament-breezy::grid.column>
         <h3 @class(['text-lg font-medium filament-breezy-grid-title'])>{{$title}}</h3>
 
         <p @class(['mt-1 text-sm text-gray-500 filament-breezy-grid-description'])>
             {{$description}}
         </p>
-    </x-grid.column>
+    </x-filament-breezy::grid.column>
 
-    <x-grid.column>
+    <x-filament-breezy::grid.column>
         {{ $slot }}
-    </x-grid.column>
+    </x-filament-breezy::grid.column>
 
-</x-grid>
+</x-filament-breezy::grid>

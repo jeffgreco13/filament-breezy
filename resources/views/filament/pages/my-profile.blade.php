31c323339094735acb110cf04cf0d08df65692af
Jacob Timmerman <75219092+Jacobtims@users.noreply.github.com>
2025-08-26 11:19:37 +0200

    Filament 4 improvements (#463)
    * Fix profile dividers
    
    * Add Tailwind classes to installation guide

--- resources/views/filament/pages/my-profile.blade.php
+++ resources/views/filament/pages/my-profile.blade.php
@@ -1,5 +1,5 @@
 <x-filament::page>
-    <div class="space-y-6 divide-y divide-gray-900/10 dark:divide-white/10">
+    <div class="divide-y divide-gray-900/10 dark:divide-white/10 [&>*:not(:first-child)]:pt-6 [&>*:not(:last-child)]:pb-6">
         @foreach ($this->getRegisteredMyProfileComponents() as $component)
             @unless(is_null($component))
                 @livewire($component)
